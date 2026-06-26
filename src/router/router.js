e68cffb776c8e8f0d40650c46033f21b5884f0d2
casmir844-wq <casmir844@gmail.com>
2026-06-26 23:19:58 +0100

    Update router.js

--- src/router/router.js
+++ src/router/router.js
@@ -1,1 +1,15 @@
+const routes = {};
 
+export function registerRoute(name, renderFunction) {
+    routes[name] = renderFunction;
+}
+
+export function navigate(route) {
+    if (!routes[route]) {
+        console.error(`Route "${route}" not found.`);
+        return;
+    }
+
+    document.querySelector("#app").innerHTML = "";
+    routes[route]();
+}
