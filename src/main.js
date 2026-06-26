770575817b944fc2ca573ed20d16b2598c5f6416
casmir844-wq <casmir844@gmail.com>
2026-06-26 23:23:19 +0100

    Refactor main.js to use router and register routes

--- src/main.js
+++ src/main.js
@@ -1,22 +1,17 @@
 import "./style.css";
-import app from "./firebase/firebase.js";
 
-console.log("✅ Firebase connected:", app);
+import { registerRoute,navigate } from "./router/router";
 
-document.querySelector("#app").innerHTML = `
-<div class="min-h-screen flex items-center justify-center p-6">
+import { loginPage } from "./pages/login";
 
-  <div class="backdrop-blur-xl bg-white/10 border border-white/20 rounded-3xl p-10 w-full max-w-md shadow-2xl">
+import { registerPage } from "./pages/register";
 
-    <h1 class="text-4xl font-bold text-center">
-      SchoolChat
-    </h1>
+import { homePage } from "./pages/home";
 
-    <p class="text-center mt-4 text-white/70">
-      Firebase Connected 🔥
-    </p>
+registerRoute("login",loginPage);
 
-  </div>
+registerRoute("register",registerPage);
 
-</div>
-`;
+registerRoute("home",homePage);
+
+navigate("login");
